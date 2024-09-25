8d87fb329b569f39ea791fa37bbb5ffa76c5ece4
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-09-25 20:57:41 +0200

    Time: 0 ms (100.00%), Space: 41.2 MB (89.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,32 +1,20 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String result = "";
-        if(strs.length<2){
-            return strs[0];
-        }
-
-        int minStringLength = minStringLength(strs);
-
-        for(int i=0; i<minStringLength; i++){
-            for(int j=1; j<strs.length; j++){
-                if(strs[j].charAt(i) != strs[0].charAt(i)){
-                    return result;
-                }
-            }
-            result += strs[0].charAt(i);
+        if(strs.length==1) return strs[0];
+        String result = strs[0];
+        for(int i=1; i<strs.length; i++){
+            result = findCommonPrefix(result,strs[i]);
         }
         return result;
     }
 
-    // get minimum string length inside array of strings
-    public int minStringLength(String[] strs){
-        int minLength = strs[0].length();
-        for(int i=0; i<strs.length; i++){
-            if(strs[i].length() < minLength){
-                minLength = strs[i].length();
+    public String findCommonPrefix(String str1, String str2){
+        int minLength = str1.length()<str2.length() ? str1.length() : str2.length();
+        for(int i=0; i<minLength; i++){
+            if(str1.charAt(i)!=str2.charAt(i)){
+                return str1.substring(0,i);
             }
         }
-        return minLength;
+        return str1.substring(0,minLength);
     }
-
 }
