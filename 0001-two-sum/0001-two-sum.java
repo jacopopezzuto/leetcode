899d30d0654a073799c2727e3d3495048016cf35
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-09-25 20:20:38 +0200

    Time: 4 ms (63.53%), Space: 44.8 MB (63.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,10 +5,11 @@ class Solution {
             map.put(nums[i],i);
         }
         for(int i=0; i<nums.length; i++){
-            if(map.containsKey(target-nums[i]) && map.get(target-nums[i])!=i){
-                return new int[] {i, map.get(target-nums[i])};
+            int difference = target-nums[i];
+            if(map.containsKey(difference) && map.get(difference)!=i){
+                return new int[]{i,map.get(difference)};
             }
         }
-        return new int[] {};
+        return new int[]{};
     }
 }
