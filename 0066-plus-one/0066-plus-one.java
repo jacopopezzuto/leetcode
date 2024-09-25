7d2c327b4887d3df13f9567e81bb3499982df64f
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-09-25 21:34:17 +0200

    Time: 0 ms (100.00%), Space: 41.4 MB (96.54%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for(int i=n-1; i>-1; i--){
-            digits[i]= (digits[i]+1)%10;
-            if(digits[i]!=0){
-                return digits;
-            }
-        }
-        int[] result = new int[n+1];
-        result[0]=1;
+       for(int i=digits.length-1; i>-1; i--){
+           digits[i] = (digits[i]+1) % 10;
+           if(digits[i]!=0){
+               return digits;
+           }
+       }
+        int[] result = new int[digits.length+1];
+        result[0] = 1;
         return result;
     }
 }
