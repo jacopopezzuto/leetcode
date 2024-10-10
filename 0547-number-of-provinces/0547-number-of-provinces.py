fa643958983b42f971435189358ab9460d388487
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-10-10 15:08:37 +0200

    Time: 190 ms (32.11%), Space: 17.3 MB (88.85%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -4,16 +4,21 @@ class Solution:
         n = len(isConnected)
         visit = [0]*n
         numberOfComponents=0
-        
-        def dfs(node,isConnected,visit):
+            
+        def bfs(node, isConnected, visit):
+            queue=deque([node])
             visit[node]=1
-            for i in range(0,n):
-                if isConnected[node][i]==1 and visit[i]==0:
-                    dfs(i,isConnected,visit)
+            while queue:
+                city=queue.popleft()
+                for i in range(0,n):
+                    if isConnected[city][i]==1 and visit[i]==0:
+                        queue.append(i)
+                        visit[i]=1
             
             
         for i in range(0,n):
             if visit[i]==0:
-                dfs(i,isConnected,visit)
                 numberOfComponents+=1
+                bfs(i, isConnected, visit)
+                
         return numberOfComponents
