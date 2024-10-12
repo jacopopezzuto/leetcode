6e3e1a34191f51e522a7f401c267c7dfdcd6eaa1
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-10-12 11:15:41 +0200

    Time: 38 ms (36.31%), Space: 16.5 MB (51.17%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,21 +1,27 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        result = []
-        min_columns, min_rows = 0, 0
-        max_column, max_rows = len(matrix[0]), len(matrix)
-        while max_column>min_columns and max_rows>min_rows:
-            for j in range(min_columns, max_column):
-                result.append(matrix[min_rows][j])
-            min_rows+=1
-            for i in range(min_rows,max_rows):
-                result.append(matrix[i][max_column-1])
-            max_column-=1
-            if min_rows < max_rows:
-                for j in range(max_column-1,min_columns-1,-1):
-                    result.append(matrix[max_rows-1][j])
-                max_rows-=1
-            if min_columns < max_column:
-                for i in range(max_rows-1,min_rows-1,-1):
-                    result.append(matrix[i][min_columns])
-                min_columns+=1
+        left,right=0,len(matrix[0])
+        top,bottom=0,len(matrix)
+        result=[]
+        while left<right and top<bottom:
+            #from left to right
+            for i in range(left,right):
+                result.append(matrix[top][i])
+            top+=1
+            #from top to bottom
+            for i in range(top,bottom):
+                result.append(matrix[i][right-1])
+            right-=1
+            
+            if not (left < right and top < bottom):
+                break
+                
+            #from right to left
+            for i in range(right-1,left-1,-1):
+                result.append(matrix[bottom-1][i])
+            bottom-=1
+            #from bottom to top
+            for i in range(bottom-1,top-1,-1):
+                result.append(matrix[i][left])
+            left+=1
         return result
