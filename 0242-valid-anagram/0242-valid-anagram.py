0efb7ed95c7decfdd4499917b072cbe97e645293
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-10-21 17:56:29 +0200

    Time: 15 ms (98.60%), Space: 16.6 MB (99.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        """
-        First approach with sort
-        """
-        return sorted(list(s)) == sorted(list(t))
+        if len(s)!=len(t):
+            return False
+        occ1,occ2 = {},{}
+        for i in s:
+            if i in occ1:
+                occ1[i]+=1
+            else:
+                occ1[i]=1
+        for i in t:
+            if i in occ2:
+                occ2[i]+=1
+            else:
+                occ2[i]=1
+        for key,value in occ1.items():
+            if key not in occ2 or value != occ2[key]:
+                return False
+        return True
+            
         
