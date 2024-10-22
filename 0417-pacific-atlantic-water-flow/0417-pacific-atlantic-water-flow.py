5afafa034bb42b58dbe8d1a5c2000a3bb2e10433
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-10-23 00:06:59 +0200

    Time: 29 ms (98.55%), Space: 18.3 MB (26.52%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,40 +1,46 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        m,n=len(heights),len(heights[0])
+        m,n = len(heights), len(heights[0])
         pacific,atlantic=[],[]
-        directions=[[1,0],[0,1],[-1,0],[0,-1]]
+        directions=[[0,1],[1,0],[-1,0],[0,-1]]
         for i in range(0,m):
             pacific.append([False]*n)
             atlantic.append([False]*n)
-        def bfs(queue,ocean):
+        
+        def bfs(queue,array):
             while queue:
                 x,y=queue.popleft()
                 for dx,dy in directions:
                     nx,ny=x+dx,y+dy
-                    if 0<=nx<m and 0<=ny<n and heights[nx][ny]>=heights[x][y] and ocean[nx][ny]!=True:
+                    if 0<=nx<m and 0<=ny<n and not array[nx][ny] and heights[nx][ny]>=heights[x][y]:
                         queue.append([nx,ny])
-                        ocean[nx][ny]=True
-        
-        pacific_q=deque()
-        for i in range(0,n):
-            pacific[0][i]=True
-            pacific_q.append([0,i])
-        for i in range(0,m):
-            pacific[i][0]=True
-            pacific_q.append([i,0])
-        bfs(pacific_q,pacific)
-        atlantic_q=deque()
-        for i in range(0,m):
-            atlantic[i][n-1]=True
-            atlantic_q.append([i,n-1])
+                        array[nx][ny]=True
             
+        pacific_queue=deque()
         for i in range(0,n):
+            pacific_queue.append([0,i])
+            pacific[0][i]=True
+            bfs(pacific_queue,pacific)
+        for i in range(0,m):
+            pacific_queue.append([i,0])
+            pacific[i][0]=True
+            bfs(pacific_queue,pacific)
+        
+        atlantic_queue=deque()
+        for i in range(0,m):
+            atlantic_queue.append([i,n-1])
+            atlantic[i][n-1]=True
+            bfs(atlantic_queue,atlantic)
+        for i in range(0,n):
+            atlantic_queue.append([m-1,i])
             atlantic[m-1][i]=True
-            atlantic_q.append([m-1,i])
-        bfs(atlantic_q,atlantic)
+            bfs(atlantic_queue,atlantic)
         result=[]
         for i in range(0,m):
             for j in range(0,n):
-                if pacific[i][j]==True and atlantic[i][j]==True:
+                if atlantic[i][j]==pacific[i][j]==True:
                     result.append([i,j])
         return result
+            
+        
+        atlantic_queue=deque()
