1f62c689a3aeded69d28a6d0035d7d339af0bbde
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-10-29 17:35:42 +0100

    Time: 3 ms (91.31%), Space: 20.2 MB (92.24%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dictionary = defaultdict(dict)
-        for num in nums:
-            if dictionary.get(num) != None :
-                dictionary.update({num : dictionary.get(num)+1})
-            else :
-                 dictionary.update({num : 1})
-        sorted_tuples = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
-        result = []
-        i=0
-        while i<k:
-            result.append(sorted_tuples[i][0])
-            i+=1
+        freq={}
+        n=len(nums)
+        for i in range(0,n):
+            if nums[i] in freq:
+                freq[nums[i]]+=1
+            else:
+                freq[nums[i]]=1
+        heap=[]
+        for key,value in freq.items():
+            heap.append((-value,key))
+        heapq.heapify(heap)
+        result=[]
+        for i in range(0,k):
+            result.append(heapq.heappop(heap)[1])
         return result
             
             
