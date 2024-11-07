da21354bb7a66b9bbf1207d362f58704257234ac
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-11-07 19:17:51 +0100

    Time: 77 ms (45.71%), Space: 18.1 MB (87.02%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,29 +2,26 @@ class Solution:
     def calculate(self, s: str) -> int:
         curr_number=0
         operation='+'
-        stack=[]
+        lastNumber=0
         operations={"+","-","*","/"}
+        result=0
         for i in range(0,len(s)):
             character=s[i]
             if character!=" " and character.isdigit():
                 curr_number=(curr_number*10)+int(character)
             if character in operations or i==len(s)-1:
-                if operation == '-':
-                    stack.append(-curr_number)
-                elif operation=='+':
-                    stack.append(curr_number)
+                if operation == '-' or operation=='+':
+                    result+=lastNumber
+                    lastNumber=curr_number if (operation == '+') else -curr_number
                 elif operation=='*':
-                    stack.append(stack.pop()*curr_number)
+                    lastNumber*=curr_number
                 elif operation=='/':
-                    top = stack.pop()
-                    if top < 0:
-                        stack.append(-(abs(top) // curr_number))
+                    if lastNumber < 0:
+                        lastNumber=-(abs(lastNumber) // curr_number)
                     else:
-                        stack.append(top // curr_number)
+                        lastNumber=(lastNumber // curr_number)
                 operation=character
                 curr_number=0
-            
-        result=0
-        while stack:
-            result+=stack.pop()
+        
+        result+=lastNumber
         return result
