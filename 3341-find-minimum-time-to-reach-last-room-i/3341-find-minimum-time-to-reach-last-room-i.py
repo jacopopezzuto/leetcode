ba5ce1b047d9ace99ec968a7ff8658505e6e0e2b
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-11-09 15:58:30 +0100

    Time: 106 ms (96.69%), Space: 17.3 MB (39.20%) - LeetHub

---
+++ 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        R=len(moveTime)
+        C=len(moveTime[0])
+        dir=[(0,1),(1,0),(-1,0),(0,-1)]
+        visited=[[False]*(C) for _ in range(R)]
+        minHeap=[(0,0,0)] #time, row, col 
+        heapq.heapify(minHeap)
+        #Heapified based on the first value i.e time, which we want
+        while minHeap:
+            time,r,c=heapq.heappop(minHeap)
+            if r==R-1 and c==C-1:
+                return time
+
+            for dr, dc in dir:
+                new_r,new_c=r+dr,c+dc
+
+                if (new_r>=R or new_c>=C or new_r<0 or new_c<0 or visited[new_r][new_c]):
+                    continue
+                
+                visited[new_r][new_c]=True
+                heapq.heappush(minHeap, (max(time+1, moveTime[new_r][new_c]+1),new_r, new_c)) 
+                #We find the max of current time+1 (time to reach adjacent room) and time (+1, same reason) in the current value of the matrix.
+        
+        return -1
