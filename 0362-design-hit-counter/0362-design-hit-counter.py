7a2f0e831dbb0de95641337d049a81c5d4af8848
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-11-09 17:48:57 +0100

    Time: 0 ms (100.00%), Space: 16.8 MB (7.68%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.py
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -4,12 +4,18 @@ class HitCounter:
         self.hits=deque()
 
     def hit(self, timestamp: int) -> None:
-        self.hits.append(timestamp)
+        if self.hits and self.hits[-1][0]==timestamp:
+            self.hits[-1]=(timestamp,self.hits[-1][1]+1)
+        else:
+            self.hits.append((timestamp,1))
 
     def getHits(self, timestamp: int) -> int:
-        while self.hits and self.hits[0]<= timestamp-300:
+        while self.hits and self.hits[0][0]<= timestamp-300:
             self.hits.popleft()
-        return len(self.hits)
+        result=0
+        for i in range(len(self.hits)):
+            result+=self.hits[i][1]
+        return result
         
 
 
