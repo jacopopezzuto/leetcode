18795239844ee76da22b19a18e2b2385198d6314
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-11-11 21:12:13 +0100

    Time: 229 ms (80.79%), Space: 19.7 MB (43.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,40 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        '''
-        Input: grid = [
-          ["1","1","1","1","0"],
-          ["1","1","0","1","0"],
-          ["1","1","0","0","0"],
-          ["0","0","0","0","0"]
-        ]
-        Output: 1
-        
-        '''
-        if not grid:
-            return 0
         m,n=len(grid),len(grid[0])
+        directions=[[0,1],[1,0],[-1,0],[0,-1]]
         count=0
-        def dfs(r, c):
-            if (
-                r < 0
-                or c < 0
-                or r >= len(grid)
-                or c >= len(grid[0])
-                or grid[r][c] != "1"
-            ):
+        check=[]
+        for i in range(m):
+            check.append([False]*n)
+        def dfs(i,j):
+            if not (0<=i<m and 0<=j<n) or check[i][j]==True or grid[i][j]!="1":
                 return 
+            check[i][j]=True
+            for dx,dy in directions:
+                dfs(i+dx,j+dy)
             
-            grid[r][c] = "2"
-
-            dfs(r - 1, c)
-            dfs(r + 1, c)
-            dfs(r, c - 1)
-            dfs(r, c + 1)
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    dfs(i, j)
-                    count += 1
-
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=="1" and check[i][j]==False:
+                    count+=1
+                    dfs(i,j)
         return count
-            
