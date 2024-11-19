09fa0fceb0c64d4eddd1cc3266fb40016935958e
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-11-19 19:20:43 +0100

    Time: 7 ms (97.32%), Space: 17.2 MB (30.60%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,38 +1,21 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        ans=[]
-        empty_board=[]
-        for i in range(n):
-            empty_board.append(["."]*n)
-        self.backtrack(0, set(), set(), set(), empty_board,n,ans)
+        def dfs(i: int):
+            if i == n:
+                ans.append(["".join(row) for row in g])
+                return
+            for j in range(n):
+                if col[j] + dg[i + j] + udg[n - i + j] == 0:
+                    g[i][j] = "Q"
+                    col[j] = dg[i + j] = udg[n - i + j] = 1
+                    dfs(i + 1)
+                    col[j] = dg[i + j] = udg[n - i + j] = 0
+                    g[i][j] = "."
+
+        ans = []
+        g = [["."] * n for _ in range(n)]
+        col = [0] * n
+        dg = [0] * (n << 1)
+        udg = [0] * (n << 1)
+        dfs(0)
         return ans
-    
-    def create_board(self, state):
-        board=[]
-        for row in state:
-            board.append("".join(row))
-        return board
-    
-    def backtrack(self, row, diagonals, anti_diagonals, cols, state,n, ans):
-        if row==n:
-            ans.append(self.create_board(state))
-            return
-        for col in range(n):
-            curr_diagonal=row-col
-            curr_anti_diagonal = row+col
-            if(col in cols or curr_diagonal in diagonals or curr_anti_diagonal in anti_diagonals):
-                continue
-            cols.add(col)
-            diagonals.add(curr_diagonal)
-            anti_diagonals.add(curr_anti_diagonal)
-            state[row][col]="Q"
-            
-            self.backtrack(row+1, diagonals, anti_diagonals, cols, state,n,ans)
-            
-            cols.remove(col)
-            diagonals.remove(curr_diagonal)
-            anti_diagonals.remove(curr_anti_diagonal)
-            state[row][col]="."
-            
-    
-    
