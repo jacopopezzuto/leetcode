dbf4909a66e4e3a2664aeb1ef6cc3840f73b8565
jacopopezzuto <34068319+jacopopezzuto@users.noreply.github.com>
2024-12-10 15:18:58 +0100

    Time: 48 ms (8.93%), Space: 26.8 MB (5.46%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,12 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        '''
+        [1,2,3,4]
+        1=24 -> 2*3*4
+        2=12 -> 1*3*4
+        3=8 -> 1*2*4
+        4=6 -> 1*2*3
+        
+        1 1*1 1*1*2 1*1*2*3 1*1*2*3*4
+      1*2*3*4*1  2*3*4*1 3*4*1 4*1 1
+        
+        1 1 2 6 24
+        24 24 12 4 1
+        
+        
+        '''
         n=len(nums)
-        result = [1]*n
-        suffix=1
-        for i in range(1,n):
-            result[i]=nums[i-1]*result[i-1]
-   
+        prefix=[0]*(n+1)
+        postfix=[0]*(n+1)
+        prefix[0]=1
+        postfix[n]=1
+        result=[0]*n
+        for i in range(1,n+1):
+            prefix[i]=prefix[i-1]*nums[i-1]
         for i in range(n-1,-1,-1):
-            result[i]=suffix*result[i]
-            suffix=nums[i]*suffix
+            postfix[i]=postfix[i+1]*nums[i]
+        print(prefix)
+        print(postfix)
+        for i in range(0,n):
+            result[i]=prefix[i]*postfix[i+1]
         return result
